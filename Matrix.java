fb7c842dadd3d18a8a39b9d5cd580d50f0d910e8
bixiphel <allen131@rangers.uwp.edu>
2025-11-27 12:35:02 -0600

    Implemented Matrix data structure.

--- Matrix.java
+++ Matrix.java
@@ -1,19 +1,31 @@
 public class Matrix {
     // Instance variables
     private int size;
+    private static int[][] matrix;
     
     // 0-arg constructor
     public Matrix() {
         size = 1;
+        matrix = new int[size][size];
     }
     
     // 1-arg constructor
     public Matrix(int size) {
         this.size = size;
+        matrix = new int[size][size];
     }
-
-    // Test main method
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    
+    // Print method
+    public static void print() {
+        System.out.println("Test!\n");
+        
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            
+            
+            }
+            System.out.println();
+        }
     }
 }
