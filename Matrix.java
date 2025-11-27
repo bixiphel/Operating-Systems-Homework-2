a23e31a4310703f5095fd5e0b35ba866848bb5c3
bixiphel <allen131@rangers.uwp.edu>
2025-11-27 14:22:03 -0600

    Updated formatting

--- Matrix.java
+++ Matrix.java
@@ -63,8 +63,6 @@ public class Matrix {
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[i].length; j++) {
                 System.out.printf("%11d%3s",matrix[i][j], "");
-            
-            
             }
             System.out.println();
         }
