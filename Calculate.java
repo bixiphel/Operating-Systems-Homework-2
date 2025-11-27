58d4f4cf2c67925ca1f935175f90722ec2b01290
bixiphel <allen131@rangers.uwp.edu>
2025-11-27 12:35:51 -0600

    Added Calculate.java to perform calculations.

---
+++ Calculate.java
@@ -0,0 +1,12 @@
+public class Calculate {
+
+    // Main method
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Matrix matrix = new Matrix(5);    
+        matrix.print();    
+    }
+
+
+}
