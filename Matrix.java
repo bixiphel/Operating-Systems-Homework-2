872026dbdbe6964c8d87ff18461ac70c4a9d0a19
bixiphel <allen131@rangers.uwp.edu>
2025-11-27 12:07:47 -0600

    Revert "Added constructors to Matrix class"
    This reverts commit c63bf4e76bd62bc4fe950954bdb12ba611341e27.

--- Matrix.java
+++ Matrix.java
@@ -1,18 +1,5 @@
 public class Matrix {
-    // Instance variables
-    private int size;
-    
-    // 0-arg constructor
-    public Matrix() {
-      size = 1;
-    }
-    
-    // 1-arg constructor
-    public Matrix(int size) {
-      this.size = size;
-    }
-    
     public static void main(String[] args) {
-      System.out.println("Hello World!");
+    System.out.println("Hello world!");
     }
 }
