504e1c45ca14b4dc3e243a6d792bd7cb18950476
bixiphel <allen131@rangers.uwp.edu>
2025-11-27 12:17:49 -0600

    Added consstructors to Matrix.java.

--- Matrix.java
+++ Matrix.java
@@ -1,5 +1,19 @@
 public class Matrix {
+    // Instance variables
+    private int size;
+    
+    // 0-arg constructor
+    public Matrix() {
+        size = 1;
+    }
+    
+    // 1-arg constructor
+    public Matrix(int size) {
+        this.size = size;
+    }
+
+    // Test main method
     public static void main(String[] args) {
-    System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
