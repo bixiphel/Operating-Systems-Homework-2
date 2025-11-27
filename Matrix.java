a80928d66b8fbd5b3a945487ddbbdfa570c2b1e3
bixiphel <allen131@rangers.uwp.edu>
2025-11-27 13:13:11 -0600

    Added an initialization constructor to set specific values

--- Matrix.java
+++ Matrix.java
@@ -3,16 +3,52 @@ public class Matrix {
     private int size;
     private static int[][] matrix;
     
-    // 0-arg constructor
+    /** 
+    * Default constructor for a matrix object. Initializes the size to 1 with a value of 0.
+    */
     public Matrix() {
         size = 1;
         matrix = new int[size][size];
     }
     
-    // 1-arg constructor
+    /** 
+    * 1-arg constructor for an integer (square) matrix object, which is a nxn matrix of integers.
+    * @param size Sets the size of the matrix. 
+    */
     public Matrix(int size) {
         this.size = size;
         matrix = new int[size][size];
+    }
+    
+    /**
+    * 1-arg constructor for initializing a matrix with values. 
+    */
+    public Matrix(String values) {
+        // Parses the string of values
+        String[] parts = values.split("-");
+        int[] numbers = new int[parts.length];
+        
+        // Adds the separated values to the 'numbers' array
+        for(int i = 0; i < parts.length; i++) {
+            numbers[i] = Integer.parseInt(parts[i]);
+        }
+        
+        // Calculates the max size of a square matrix using the provided values. This always rounds down, so if the string of provided values isn't a perfect square, some values will be lost
+        int maxSize = (int)Math.sqrt(numbers.length);
+        
+        size = maxSize;
+        matrix = new int[size][size];
+        
+        // This is the index of the 'values' array
+        int k = 0;
+        
+        // Adds the kth element to the matrix object left-to-right, top-to-bottom.
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = numbers[k++];
+            }
+        }
+        
     }
     
     // Print method
