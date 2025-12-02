370faae4d0cddbe73d17e362e0788b13d248df7a
bixiphel <allen131@rangers.uwp.edu>
2025-12-02 16:50:42 -0600

    Implemented a structure to store fixed values in the matrix and helpful associated methods

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        Worker thread1 = new Worker();
+        thread1.start();
     }
 }
