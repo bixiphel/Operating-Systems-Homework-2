3c3037499c5f822b3653d1644b0b69bcd3a7bdf4
bixiphel <allen131@rangers.uwp.edu>
2025-12-02 18:24:53 -0600

    Initialized thread class

--- Worker.java
+++ Worker.java
@@ -1,1 +1,10 @@
-public class Worker {}
+public class Worker extends Thread {
+    @Override
+    public void run() {
+        // This is for the "getId()" method, which was deprecated at some point. This is a small project so it doesn't matter.
+        @SuppressWarnings("deprecation")
+        long threadID = getId();
+        
+        System.out.println("Hello from thread " + threadID + "!");
+    }
+}
